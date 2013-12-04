b70f44f7afaf9e884a6f052f28a99c556eb8de91
Thibault B <thibertrand@hotmail.fr>
2013-12-04 10:58:53 +0100

    Amélioration de la page d'un groupe : Sum41 et du style.php

--- Sum 41.php
+++ Sum 41.php
@@ -13,22 +13,41 @@
 		<br>Nationalité : Canadienne
 		<br>Nombres de membres : 4
 		<br>Nombres d'albums : 6
+		<br>Genres : Punk, Rock, Metal
+		<br>Instruments : Guitare, Basse, Batterie, Piano, Voix
+		<br>
 		
+	</div>
 		
+	<br>
+	
+	<div id="NbAlbums"><h4> Albums :</h4>
+		2000 : Half Hour Of Power
+		<br>2001 : All Killer, No Filler
+		<br>2002 : Does This Look Infected ?
+		<br>2004 : Chuck
+		<br>2007 : Underclass Hero
+		<br>2011 : Screaming Bloody Murder
+		
+	
 	</div>
 	
 	<br>
 	
-	<div id="NbAlbums"><h4> Albums :</h4>
-		2000 : Half hour of power
-		<br>2001 : All killer, no filler
-		<br>le troisième
+	<object align = "left" width="520" height="380"><param name="movie" value="http://www.youtube.com/v/xp2FG5EwHpo"></param><param name="wmode" value="transparent"></param><embed src="http://www.youtube.com/v/xp2FG5EwHpo" type="application/x-shockwave-flash" wmode="transparent" width="520" height="380"></embed></object>
+
+	<br>
+	
+	<div id="Notation10"><h4> Notation :  10/10 </h4>
 	
 	</div>
 	
+	<br>
+	
+	<div id="com"><h4> Commentaire :</h4>
 	
 	
-	<object width="520" height="380"><param name="movie" value="http://www.youtube.com/v/xp2FG5EwHpo"></param><param name="wmode" value="transparent"></param><embed src="http://www.youtube.com/v/xp2FG5EwHpo" type="application/x-shockwave-flash" wmode="transparent" width="520" height="380"></embed></object> 
+	</div>
 	
 </p>
 <?php
